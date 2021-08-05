a5f2b37e05a5055e9f33d840076ea21979c4282f
CesarManalac <cesar_manalac@dlsu.edu.ph>
2021-08-06 01:19:47 +0800

    Added Reflection Probes to all rooms except room 2

--- GDFUNDA - Loss/Assets/Scripts/Manager/SceneController.cs
+++ GDFUNDA - Loss/Assets/Scripts/Manager/SceneController.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour
 {
-    public void LoadMainMenu()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
-
     public void LoadGameScene()
     {
         SceneManager.LoadScene("Preload");
