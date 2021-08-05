aa0f77675055743c6872d75cde0e08bfbecc5b12
melon_pain <james_omalin@dlsu.edu.ph>
2021-08-05 20:29:49 +0800

    Fixed scene loader

--- GDFUNDA - Loss/Assets/Scripts/SceneLoader.cs
+++ GDFUNDA - Loss/Assets/Scripts/SceneLoader.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField] private string scene;
