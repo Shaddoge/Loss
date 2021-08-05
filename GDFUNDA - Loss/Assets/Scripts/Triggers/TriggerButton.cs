5289f10f1c420e0f5265fbfca288fe6e90d520dd
John Santos <john_adriel_santos@dlsu.edu.ph>
2021-08-06 02:45:27 +0800

    Added Light Probes. Adjusted Reset Time Pressure Plate

--- GDFUNDA - Loss/Assets/Scripts/Triggers/TriggerButton.cs
+++ GDFUNDA - Loss/Assets/Scripts/Triggers/TriggerButton.cs
@@ -79,7 +79,7 @@ public class TriggerButton : MonoBehaviour
 
     private IEnumerator ResetPlateTimer()
     {
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(1.0f);
         resetPressurePlate.ResetPlate();
     }
 }
